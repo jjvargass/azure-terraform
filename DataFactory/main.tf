8bbd0d3fc656ffa7b0cd2ba7341d0d766b7176b0
jjvargass <jotavargas89@gmail.com>
2021-10-04 18:33:56 -0500

    fix:nueva estructura de nombres para los recursos de azure

--- DataFactory/main.tf
+++ DataFactory/main.tf
@@ -15,17 +15,17 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 # Create a resource group
-resource "azurerm_resource_group" "rg-sdssandbox-dev-eastUS-001" {
-  name     = "rg-sdssandbox-dev-eastUS-001"
+resource "azurerm_resource_group" "sdssandbox-rg01-dev-eastUS" {
+  name     = "sdssandbox-rg01-dev-eastUS"
   location = "East  US"
 }
 # -- --
 # Key Vault
 # -- --
-resource "azurerm_key_vault" "kv-sdssandbox-dev" {
-  name                = "kv-sdssandbox-dev"
-  location            = azurerm_resource_group.rg-sdssandbox-dev-eastUS-001.location
-  resource_group_name = azurerm_resource_group.rg-sdssandbox-dev-eastUS-001.name
+resource "azurerm_key_vault" "sdssandbox-kv01-dev-eastUS" {
+  name                = "sdssandbox-kv01-dev-eastUS"
+  location            = azurerm_resource_group.sdssandbox-rg01-dev-eastUS.location
+  resource_group_name = azurerm_resource_group.sdssandbox-rg01-dev-eastUS.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 
@@ -36,8 +36,8 @@ resource "azurerm_key_vault" "kv-sdssandbox-dev" {
 }
 
 # Key Vault Access Policy del creador el key Vault (Cliente).
-resource "azurerm_key_vault_access_policy" "kvap-sdssandbox-dev-client" {
-  key_vault_id = azurerm_key_vault.kv-sdssandbox-dev.id
+resource "azurerm_key_vault_access_policy" "sdssandbox-kvap01-dev-client" {
+  key_vault_id = azurerm_key_vault.sdssandbox-kv01-dev-eastUS.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
@@ -101,12 +101,12 @@ resource "azurerm_key_vault_access_policy" "kvap-sdssandbox-dev-client" {
 
 # -- --
 # Storage Account
-# El nombre solo permite minusculas y numeros | st-sdssandbox-dev
+# El nombre solo permite minusculas y numeros | sdssandbox-st01-dev-eastUS => sdssandboxst01deveastus
 # -- --
-resource "azurerm_storage_account" "stsdssandboxdev" {
-  name                     = "stsdssandboxdev"
-  resource_group_name      = azurerm_resource_group.rg-sdssandbox-dev-eastUS-001.name
-  location                 = azurerm_resource_group.rg-sdssandbox-dev-eastUS-001.location
+resource "azurerm_storage_account" "sdssandboxst01deveastus" {
+  name                     = "sdssandboxst01deveastus"
+  resource_group_name      = azurerm_resource_group.sdssandbox-rg01-dev-eastUS.name
+  location                 = azurerm_resource_group.sdssandbox-rg01-dev-eastUS.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -120,25 +120,32 @@ resource "azurerm_storage_account" "stsdssandboxdev" {
 
 }
 
-# Container
-resource "azurerm_storage_container" "dls-sdssandbox-dev-empleados-fuente" {
-  name                  = "dls-sdssandbox-dev-empleados-fuente"
-  storage_account_name  = azurerm_storage_account.stsdssandboxdev.name
+# Container - Data Lake Store
+resource "azurerm_storage_container" "sdssandbox-dls01-dev-eastUS" {
+  name                  = "sdssandbox-dls01-dev-eastUS"
+  storage_account_name  = azurerm_storage_account.sdssandboxst01deveastus.name
   container_access_type = "private"
 }
 
-# access policy del storage container
-resource "azurerm_key_vault_access_policy" "kvap-sdssandbox-dev-storage-container" {
-  key_vault_id = azurerm_key_vault.kv-sdssandbox-dev.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_storage_account.stsdssandboxdev.identity.0.principal_id
+# access policy del storage container (creo que no es necesario)
 
-  secret_permissions = [
-    "get",
-    "list",
-  ]
+# resource "azurerm_key_vault_access_policy" "sdssandbox-kvap01-dev-eastUS" {
+#   key_vault_id = azurerm_key_vault.sdssandbox-kv01-dev-eastUS.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = azurerm_storage_account.sdssandboxst01deveastus.identity.0.principal_id
+
+#   secret_permissions = [
+#     "get",
+#     "list",
+#   ]
+# }
+
+# Secreto - connection string de storage container en Key Vault
+resource "azurerm_key_vault_secret" "sdssandbox-sect01-dev-connection-string-st01" {
+  name         = "sdssandbox-sect01-dev-connection-string-st01"
+  value        = azurerm_storage_account.sdssandboxst01deveastus.primary_connection_string
+  key_vault_id = azurerm_key_vault.sdssandbox-kv01-dev-eastUS.id
 }
-
 
 # -- --
 # eee
