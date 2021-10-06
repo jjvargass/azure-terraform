2889802f38c3da877325512dd1edb6751244ad4f
jjvargass <jotavargas89@gmail.com>
2021-10-06 17:41:10 -0500

    fix:se cambias nombres de key Vault y storage account, ya usados

--- DataFactory/main.tf
+++ DataFactory/main.tf
@@ -28,8 +28,8 @@ resource "azurerm_resource_group" "sds-rg01-dev-eastus" {
 # -- --
 # Key Vault
 # -- --
-resource "azurerm_key_vault" "sds-kv01-dev-eastus" {
-  name                = "sds-kv01-dev-eastus"
+resource "azurerm_key_vault" "sds-kv02-dev-eastus" {
+  name                = "sds-kv02-dev-eastus"
   location            = azurerm_resource_group.sds-rg01-dev-eastus.location
   resource_group_name = azurerm_resource_group.sds-rg01-dev-eastus.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -105,10 +105,10 @@ resource "azurerm_key_vault" "sds-kv01-dev-eastus" {
 
 # -- --
 # Storage Account
-# El nombre solo permite minusculas y numeros | sds-st01-dev-eastus => sdsst01deveastus
+# El nombre solo permite minusculas y numeros | sds-st01-dev-eastus => sdsst02deveastus
 # -- --
-resource "azurerm_storage_account" "sdsst01deveastus" {
-  name                     = "sdsst01deveastus"
+resource "azurerm_storage_account" "sdsst02deveastus" {
+  name                     = "sdsst02deveastus"
   resource_group_name      = azurerm_resource_group.sds-rg01-dev-eastus.name
   location                 = azurerm_resource_group.sds-rg01-dev-eastus.location
   account_tier             = "Standard"
@@ -127,15 +127,15 @@ resource "azurerm_storage_account" "sdsst01deveastus" {
 # Container - Data Lake Store
 resource "azurerm_storage_container" "sds-dls01-dev-eastus" {
   name                  = "sds-dls01-dev-eastus"
-  storage_account_name  = azurerm_storage_account.sdsst01deveastus.name
+  storage_account_name  = azurerm_storage_account.sdsst02deveastus.name
   container_access_type = "private"
 }
 
 # Secreto - connection string de storage container en Key Vault
 resource "azurerm_key_vault_secret" "sds-sect01-dev-connection-string-st01" {
   name         = "sds-sect01-dev-connection-string-st01"
-  value        = azurerm_storage_account.sdsst01deveastus.primary_connection_string
-  key_vault_id = azurerm_key_vault.sds-kv01-dev-eastus.id
+  value        = azurerm_storage_account.sdsst02deveastus.primary_connection_string
+  key_vault_id = azurerm_key_vault.sds-kv02-dev-eastus.id
 }
 
 # -- --
